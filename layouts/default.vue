d175f705d1074f91c480ea6bb0ea488f84f07458
Divine Azianka <aziankad@gmail.com>
2022-12-12 19:16:29 +0000

    Update of default layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="shadow-sm bg-blue">
       <nav class="container mx-auto p-4 flex justify-between">
-        <NuxtLink to="/">Dojo Mersh</NuxtLink>
+        <NuxtLink to="/" class="font-bold">Dojo Mersh</NuxtLink>
 
         <ol class="flex gap-4">
           <li><NuxtLink to="/">Home</NuxtLink></li>
