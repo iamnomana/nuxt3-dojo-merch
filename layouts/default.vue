18cb454520eb4b1bb7f031a6b7a8d4b11b5a98d2
Divine Azianka <aziankad@gmail.com>
2022-12-12 18:49:37 +0000

    Default layout

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <header class="shadow-sm bg-blue">
+      <nav class="container mx-auto p-4 flex justify-between">
+        <NuxtLink to="/">Dojo Mersh</NuxtLink>
+
+        <ol class="flex gap-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/products">Products</NuxtLink></li>
+          <li><NuxtLink to="/about-us">About</NuxtLink></li>
+        </ol>
+      </nav>
+    </header>
+
+    <div class="container mx-auto p-4">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<style>
+  .router-link-exact-active {
+    color: #7145e7;
+  }
+</style>
