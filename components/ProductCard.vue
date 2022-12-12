40ac55a17694fc261b0a394c43dc50ff26d2741e
Divine Azianka <aziankad@gmail.com>
2022-12-12 18:49:25 +0000

    Created ProductCard and Applied it to Product Page

---
+++ components/ProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card text-center">
+    <img :src="product.image" alt="Product Img" class="thumb" />
+    <div class="font-bold text-gray-500 m-4 truncate">{{ product.title }}</div>
+    <NuxtLink :to="`/products/${product.id}`">
+      <div class="btn my-4">View</div>
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup>
+  const { product } = defineProps(["product"]);
+</script>
+ <style scoped>
+  .thumb {
+    max-height: 120px;
+    max-width: 70%;
+    margin: 0 auto;
+  }
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <div class="grid grid-cols-4 gap-5">
+      <div v-for="product in products" :key="product.id">
+        <NuxtLink :to="`/products/${product.id}`">
+          <ProductCard :product="product" />
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  // Fetch Products
+  const { data: products } = await useFetch("https://fakestoreapi.com/products");
+</script>
