a95ba12b0bfffb0c470d7292d95980f854bcf7f3
Divine Azianka <aziankad@gmail.com>
2022-12-12 19:16:13 +0000

    Created ProductDetail component and Applied it to individual Product Page

---
+++ components/ProductDetail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card">
+    <div class="grid grid-cols-2 gap-10">
+      <div class="p-7">
+        <img :src="product.image" alt="Product Image" class="mx-auto my-7" />
+      </div>
+
+      <div class="p-7">
+        <h2 class="text-4xl my-7">
+          {{ product.title }}
+        </h2>
+
+        <div class="div text-4xl my-7">Price - ${{ product.price }}</div>
+        <h3 class="font-bold border-b-2 mb-4 pb-2">Description</h3>
+        <p class="mb-7">{{ product.description }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  const { product } = defineProps(["product"]);
+</script>
+
+<style  scoped>
+  img {
+    max-width: 500px;
+  }
+</style>
---
+++ pages/products/[id].vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ProductDetail :product="product" />
+  </div>
+</template>
+
+<script setup>
+  const { id } = useRoute().params;
+
+  const { data: product } = await useFetch(
+    `https://fakestoreapi.com/products/${id}`,
+    { key: id }
+  );
+
+  // Throw Error
+  if (!product.value) {
+    throw createError({ statusCode: 404, statusMessage: "Product not Found" });
+  }
+</script>
+
+<style>
+</style>
